83c3607396f360f335b5a72ed743ea10ee283b89
Krzysztof Mikolajski <krzysztof.mikolajski1@gmail.com>
2021-02-06 01:19:21 +0100

    Creating project on start.spring.io and initializing in Intellij.

---
+++ src/main/java/TravelBookingPlatform/TravelBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package TravelBookingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookingPlatformApplication.class, args);
+	}
+
+}
